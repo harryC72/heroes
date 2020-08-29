9b18474aeaffda5561fd7b519d95b20a16a36d00
harald carlsten <harald.carlsten@betssongroup.com>
2020-08-29 16:35:21 +0200

    committing before pushing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Batman' },
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'Green Arrow' },
+  { id: 14, name: 'Flash' },
+  { id: 15, name: 'Robin' },
+  { id: 16, name: 'Aquaman' },
+  { id: 17, name: 'John Constantine' },
+];
